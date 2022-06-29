ba0832e21f31f89ed688b37b41fb8cf5dd0fd804
Light <47911535+LightAPIs@users.noreply.github.com>
2022-06-29 23:44:24 +0800

    fix: 修复歌名中包含"❤"符号时可能没有匹配结果的问题

--- src/qqSource.php
+++ src/qqSource.php
@@ -106,8 +106,8 @@ class AumQQSource {
     private function getCleanStr($str) {
         $lowStr = strtolower($str);
         return str_replace(
-            array(" ", "，", "：", "；", "！", "？", "「", "」", "（", "）", "。"),
-            array("", ",", ":", ";", "!", "?", "｢", "｣", "(", ")", "."),
+            array(" ", "，", "：", "；", "！", "？", "「", "」", "（", "）", "。", "❤"),
+            array("", ",", ":", ";", "!", "?", "｢", "｣", "(", ")", ".", "♥"),
             $lowStr);
     }
 }
